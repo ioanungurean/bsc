e810cef1460764e0444bbf4c736c89bbdba0d77c
Ioan Ungurean <inungurean@gmail.com>
2017-06-22 14:57:52 +0300

    Removing console logs

--- client/src/modules/character/index.js
+++ client/src/modules/character/index.js
@@ -4,7 +4,6 @@ export class Character {
   constructor(prototype, param, camera) {
     this.camera = camera;
     this.object = prototype.clone();
-    this.object.position.y = 10;
 
     this.moveKey = {
       up: false,
@@ -34,28 +33,24 @@ export class Character {
           if (!fired.up) {
             fired.up = true;
             this.moveKey.up = true;
-            console.log('up', this.moveKey.up);
           }
           break;
         case 65: // a
           if (!fired.left) {
             fired.left = true;
             this.moveKey.left = true;
-            console.log('left', this.moveKey.left);
           }
           break;
         case 83: // s
           if (!fired.down) {
             fired.down = true;
             this.moveKey.down = true;
-            console.log('down', this.moveKey.down);
           }
           break;
         case 68: // d
           if (!fired.right) {
             fired.right = true;
             this.moveKey.right = true;
-            console.log('right', this.moveKey.right);
           }
           break;
       }
@@ -67,28 +62,24 @@ export class Character {
           if (fired.up) {
             fired.up = false;
             this.moveKey.up = false;
-            console.log('up', this.moveKey.up);
           }
           break;
         case 65: // a
           if (fired.left) {
             fired.left = false;
             this.moveKey.left = false;
-            console.log('left', this.moveKey.left);
           }
           break;
         case 83: // s
           if (fired.down) {
             fired.down = false;
             this.moveKey.down = false;
-            console.log('down', this.moveKey.down);
           }
           break;
         case 68: // d
           if (fired.right) {
             fired.right = false;
             this.moveKey.right = false;
-            console.log('right', this.moveKey.right);
           }
           break;
       }
@@ -111,6 +102,7 @@ export class Character {
       }
     });
 
+    this.object.position.y = 10;
     this.object.position.x = start.x;
     this.object.position.z = start.z;
   }
@@ -137,8 +129,7 @@ export class Character {
       let collision = item.intersectsBox(currentBoundingBox);
 
       if (collision) {
-        console.log('DING');
-        // this.move(true);
+        this.move(true);
       }
     });
   }
@@ -157,20 +148,21 @@ export class Character {
       if (this.moveKey.right) {
         this.object.position.z += 1;
       }
+    } else {
+      if (this.moveKey.up) {
+        this.object.position.x -= 1;
+      }
+      if (this.moveKey.down) {
+        this.object.position.x += 1;
+      }
+      if (this.moveKey.left) {
+        this.object.position.z += 1;
+      }
+      if (this.moveKey.right) {
+        this.object.position.z -= 1;
+      }
     }
-    if (this.moveKey.up) {
-      this.object.position.x -= 1;
-    }
-    if (this.moveKey.down) {
-      this.object.position.x += 1;
-    }
-    if (this.moveKey.left) {
-      this.object.position.z += 1;
-    }
-    if (this.moveKey.right) {
-      this.object.position.z -= 1;
-    }
-    // this.checkCollision();
+    this.checkCollision();
   }
 
   update() {
--- client/src/modules/character/prototype/character.js
+++ client/src/modules/character/prototype/character.js
@@ -14,7 +14,7 @@ export class CharacterPrototype {
   generate() {
     return this.load().then((resources) => {
       let boxCharacter = new Mesh(
-        new BoxBufferGeometry(20, 20, 20),
+        new BoxBufferGeometry(10, 20, 10),
         new MeshBasicMaterial({ color: 0xfff222 })
       );
       boxCharacter.name = 'box';
